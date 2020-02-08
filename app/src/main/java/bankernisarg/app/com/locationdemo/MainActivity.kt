9c97ca55d7eafa6f787cdb77c3a80b8b64ec6ea2
nisarg <bankernisarg@gmail.com>
2020-02-08 16:55:34 +0530

    Initial commit

---
+++ app/src/main/java/bankernisarg/app/com/locationdemo/MainActivity.kt
@@ -0,0 +1,12 @@
+package bankernisarg.app.com.locationdemo
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivitgity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
